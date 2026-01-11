28543bebbcd1f24480fb6f2c7f7a332d6c016e6f
lcmm <lcmbook55@gmail.com>
2026-01-11 18:23:20 +0900

    실습과제 - 변수상수 작성

---
+++ JavaScript-homework/01.Variable/Variable.js
@@ -0,0 +1,97 @@
+// # 실습 과제
+
+// 변수, 상수 작성 과제
+// 각 항목에 대해 변수나 상수를 선언하고, 알맞은 값을 할당하세요.
+
+// 1. 영화 제목을 저장할 변수를 선언하고 좋아하는 영화 제목을 할당하세요.
+let movieTitle = 'Django Unchained'
+
+//테스트 코드  
+console.log(movieTitle);
+
+// 2. "빛의 속도(299,792,458 m/s)"를 담을 상수를 선언하세요.
+const SPEED_OF_LIGHT_NUMBER = 299792458
+
+//테스트 코드  
+console.log(SPEED_OF_LIGHT_NUMBER + ' m/s', typeof SPEED_OF_LIGHT_NUMBER);
+
+
+const SPEED_OF_LIGHT_STRING = '빛의 속도(299,792,458 m/s)'
+
+//테스트 코드  
+console.log(SPEED_OF_LIGHT_STRING);
+console.log(SPEED_OF_LIGHT_STRING, typeof SPEED_OF_LIGHT_STRING);
+
+
+// 3. "이메일 인증 완료 여부"를 확인하는 불리언 타입 변수를 선언하고 적절한 값을 할당하세요.
+let isEmailVerified = true
+
+//테스트 코드  
+console.log(isEmailVerified, typeof isEmailVerified);
+
+
+// 4. "상품 재고 수량(예: 47개)"을 담는 변수를 선언하고 숫자 값을 할당하세요.
+let productStockQuantity = 47
+
+//테스트 코드  
+console.log(productStockQuantity + '개', typeof productStockQuantity);
+
+
+// 5. "회원 포인트(예: 15,800점)"를 담는 변수를 작성하세요.
+let memberPoints = 15800
+
+//테스트 코드  
+console.log(memberPoints.toLocaleString() +'점', typeof memberPoints);
+
+
+// 6.  API 서버의 "기본 URL(예: https://api.example.com)을 담는 상수를 작성하고 값을 할당하세요.
+const basic_Url = 'https://api.example.com'
+
+//테스트 코드  
+console.log(basic_Url, typeof basic_Url);
+
+
+// 7.  게시글의 "조회수(예: 1,234)"를 담는 변수를 선언하고 값을 할당하세요.
+let post_Views = Number(1234)
+
+//테스트 코드  
+console.log(post_Views);
+
+
+// 8.  "배송 상태('준비중', '배송중', '배송완료' 중 하나)"를 담는 변수를 작성해보세요.
+let delivery_status = '상품준비중'
+delivery_status = '배송중'
+delivery_status = '배송완료'
+
+//테스트 코드  
+console.log(delivery_status, typeof delivery_status);
+
+
+// 9.  "쿠폰 사용 가능 여부"를 담는 변수를 선언하고 불리언 타입 값을 설정하세요.
+let is_Coupon_Available = false
+
+//테스트 코드  
+console.log(is_Coupon_Available);
+
+
+// 10. "최대 업로드 파일 크기(예: 10MB를 바이트 단위로 10485760)"를 담는 상수를 작성하세요.
+const MAX_UPLOAD_FILE_SIZE = 10485760 //20MB
+
+//테스트 코드  
+console.log(MAX_UPLOAD_FILE_SIZE , typeof MAX_UPLOAD_FILE_SIZE);
+
+
+// 11. "사용자 등급 점수(예: 85.5점)"를 담는 변수를 선언하고 소수점이 포함된 값을 할당하세요.
+let user_Level_Score = 85.5
+
+//테스트 코드  
+console.log(user_Level_Score);
+
+
+// 12. "알림 수신 동의 여부"를 담는 변수를 선언하고 불리언 타입 값을 설정하세요.
+let allow_Notification = false 
+//테스트 코드  
+console.log(allow_Notification);
+
+
+//---
